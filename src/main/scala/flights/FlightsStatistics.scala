f7935396a4ee1f37e6ec056549f7eb3e973cc072
Dima Diachenko <dmytro.diachenko@gmail.com>
2018-01-26 12:14:55 +0200

    added specs for statistics methods

--- src/main/scala/flights/FlightsStatistics.scala
+++ src/main/scala/flights/FlightsStatistics.scala
@@ -2,8 +2,19 @@ package flights
 
 object FlightsStatistics {
 
+  /**
+    * Computes total arrival per airport.
+    * @param flights all flights
+    * @return map of (airport -> total arrivals to that airport). Does NOT contain airports that have no incoming flights
+    */
   def arrivalsPerAirport(flights: Flights): Map[Airport, Int] = flights.groupBy(_.destination).mapValues(_.size)
 
+  /**
+    * Collects all airports
+    *
+    * @param flights all flights
+    * @return set of all airports that are either origin or destination in flights data
+    */
   def allAirports(flights: Flights): Set[Airport] = flights.foldLeft(Set.empty[Airport])((set, flight) =>
     set + flight.origin + flight.destination)
 
